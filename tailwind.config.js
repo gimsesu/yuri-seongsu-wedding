eb5597444d8d74b264444bfc13fe64e0e80b5332
liter.1(김성수)/kakao <liter.1@kakaocorp.com>
2024-08-11 22:09:49 +0900

    modify letter-spacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,10 +14,6 @@ module.exports = {
       flexBasis: {
         "3/25": "12%",
       },
-      padding: {
-        // 0.3rem
-        1.25: "0.3rem",
-      },
       borderWidth: {
         0.5: "0.5px",
       },
